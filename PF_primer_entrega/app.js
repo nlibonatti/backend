aa49d3c66bb688fd8483c41b9427898eec0d3a5e
nlibonatti <106163604+nlibonatti@users.noreply.github.com>
2023-01-30 18:15:49 -0300

    Inport routes

--- PF_primer_entrega/app.js
+++ PF_primer_entrega/app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import ProductManager from './ProductManager.js'
-import productsRouter from  './routes/products.router'
-import cartsRouter from './routes/carts.router'
+import productsRouter from  './routes/products.router.js'
+import cartsRouter from './routes/carts.router.js'
 
 const app = express()
 app.use(express.json())
@@ -9,58 +9,9 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
 
-
 const productManager = new ProductManager('/files/products.json')
 
 // endpoints
-
-// Products
-
-// products all
-
-app.get('/products',(req, res) => {
- 
-  const products = productManager.getProducts(req.query)
-  res.json({ message: 'productos encontrados', products })
-
-})
-
-// product one
-
-app.get('/products/:idProduct', (req, res) => {
-  const { idProduct } = req.params
-  const product = productManager.getProductById(parseInt(idProduct))
-  if(product){
-      res.json({ message: 'producto encontrado', product })
-  } else {
-    res.status(400).send('Usuario no existe')
-  }
-})
-
-// add product
-
-app.post('/product',(req,res)=>{
-    const product = req.body
-    console.log(product)
-    productManager.addProduct(product)
-    res.send('Producto agregado')
-}
-)
-
-// delete products
-
-app.delete('/product/:idProduct',(req,res)=>{
-    const {idProduct} = req.params
-    productManager.deleteProduct(parseInt(idProduct))
-    res.send('Product eliminado')
-})
-
-// delete all products
-
-app.delete('/product',(req,res)=>{
-    productManager.deleteAllProducts(req.query)
-    res.send('Usuarios eliminados')
-})
 
 app.listen(8080, () => {
   console.log('Servidor puerto 8080')
