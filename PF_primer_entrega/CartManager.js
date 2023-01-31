4312b47279a02347d141eff21c411a8abb4d0126
nlibonatti <106163604+nlibonatti@users.noreply.github.com>
2023-01-31 15:28:43 -0300

    add cartManager

---
+++ PF_primer_entrega/CartManager.js
@@ -0,0 +1,73 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const fs = require("fs");
+
+const path = './files/carts.json'
+
+
+export default class CartManager{
+    constructor(){
+        this.carts = []
+    }
+        getCart(){
+            if (fs.existsSync(path)) {
+                const carts = JSON.parse(fs.readFileSync(path, "utf-8"))
+                console.log("existe carrito")
+                return carts
+            } 
+            else {
+                console.log("no existe carrito")
+                }
+        }
+
+        addCart(){
+
+            const cart = {
+                    id: this.#addId(),
+                    product: []
+                }
+                const cartFile = this.getCart()
+                productsFile.push(cart)
+                fs.writeFileSync(path, JSON.stringify(cartFile));
+                    
+                }   
+
+        addCart(CartNew){
+        const { title, quantity, idProduct } = CartNew
+
+        if(!title || !quantity || !idProduct){
+            console.log('Falta campo')} 
+            else {
+
+            const cart = {
+                id: this.#addId(),
+                title,
+                quantity,
+                idProduct
+            }
+            const cartFile = this.getCart()
+            productsFile.push(cart)
+            fs.writeFileSync(path, JSON.stringify(cartFile));
+                
+            }   
+        } 
+
+        getCartById(id){
+            const carts = this.getCart();
+            return (carts.find(cart => cart.id === id)) || 'Error: Carrito no encontrado'
+        }
+
+        checkCode(cCode) {
+            const carts = this.getCart()
+            return carts.find((cart) => cart.code === cCode)
+        }
+
+        #addId(){
+            let id = 1
+            const carts = this.getCart()
+            if (carts.length!==0){
+                id = carts[carts.length-1].id + 1
+            }
+            return id
+        }
+}
--- PF_primer_entrega/routes/carts.router.js
+++ PF_primer_entrega/routes/carts.router.js
@@ -1,14 +1,28 @@
 import { Router } from "express"; 
+import CartManager from "../CartManager.js";
+
+const cartManager = new CartManager('/files/carts.json')
 
 const router = Router()
 
-router.post('/', (req,res) =>{
+router.post('/',(req,res)=>{
+    const cart = req.body
+    console.log(cart)
+    productManager.addCart(cart)
+    res.send('Producto agregado al carrito')
+}
+)
 
-})
+router.get('/:cid', (req, res) => {
+    const { idCart } = req.params
+    const cart = CartManager.getCarById(parseInt(idCart))
+    if(product){
+        res.json({ message: 'producto encontrado', cart })
+    } else {
+      res.status(400).send('Usuario no existe')
+    }
+  })
 
-router.get('/:cid', (req,res) =>{
-
-})
 
 router.post('/:cid/product/:pid ', (req,res) =>{
 
